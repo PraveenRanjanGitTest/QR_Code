ba5b4f19e9cb01285903b235290a263b2f83ff87
PraveenRanjanGitTest <66916763+PraveenRanjanGitTest@users.noreply.github.com>
2023-01-02 13:25:42 +0530

    Add files via upload

---
+++ Dttl.Qr.Repository/IQRDetailService.cs
@@ -0,0 +1,18 @@
+﻿using Dttl.Qr.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dttl.Qr.Repository
+{
+    public interface IQRDetailService
+    {
+        public Task<List<QRDetails>> GetQRDetailList();
+        public Task<QRDetails> GetQRDetailListById(int Id);
+        public Task<QRDetails> AddQRDetails(QRDetails qRTemplate);
+        public Task<QRDetails> UpdateQReDetails(QRDetails qRTemplate);
+        public Task<QRDetails> DeleteQRDetails(int Id);
+    }
+}
---
+++ Dttl.Qr.Repository/IVCardService.cs
@@ -0,0 +1,19 @@
+﻿using Dttl.Qr.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dttl.Qr.Repository
+{
+    public interface IVCardService
+    {
+        public Task<List<VCardQRCode>> GetVCardList();
+        public Task<VCardQRCode> GetVCardById(int Id);
+        public Task<VCardQRCode> AddVCard(VCardQRCode vCardQRCode);
+        public Task<VCardQRCode> UpdateVCarde(VCardQRCode vCardQRCode);
+        public Task<VCardQRCode> DeleteVCard(int Id);
+
+    }
+}
