03f49081b9130f11d9fef436846c7c04980038a9
Senthil.Gunasekaran <Senthil.Gunasekaran@KANINI-LTP-877>
2023-01-05 12:38:34 +0530

    React VS basecode

--- dttl.qr.presentation/src/App.tsx
+++ dttl.qr.presentation/src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TemplateComponent from './TemplateComponent'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App">
+
+          <TemplateComponent forecolor={'Hello Qr'} bgcolor={''} size={12} profilephoto={''} name={''}/>
+         
+    
     </div>
   );
 }
