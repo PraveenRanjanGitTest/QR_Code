b92c28bd349c4ed56ce0f2f35ccfe021aca96a18
Senthil.Gunasekaran <Senthil.Gunasekaran@KANINI-LTP-877>
2023-01-11 16:13:24 +0530

    UI Changes

--- dttl.qr.presentation/src/App.tsx
+++ dttl.qr.presentation/src/App.tsx
@@ -8,21 +8,22 @@ import { downloadQrCode } from './Utils/CanvasUtils';
 function App() {
     return (
         <><div className="App">
-            <svg id="testDownload" width="100" height="100">
-                <circle cx="50" cy="50" r="40" stroke="green" stroke-width="4" fill="yellow" />
-            </svg>
-
-        </div>
+            <div>
+                <svg id="testDownload" width="100" height="100">
+                    <circle cx="50" cy="50" r="40" stroke="green" stroke-width="4" fill="yellow" />
+                </svg>
+                <button onClick={() => { downloadQrCode("testDownload", "svg"); }}>Download</button>
+            </div>
+            <br/>
             <div>
                 <TemplateComponent />
-                <VCardComponent />
-
             </div>
-            <button onClick={() => {
-                downloadQrCode("testDownload", "svg"); // png, jpeg and svg
-            }
-            }>Download</button>
-
+            <br />
+            <div>
+                <VCardComponent />
+            </div>
+            <br />
+        </div>
         </>
 
     );
