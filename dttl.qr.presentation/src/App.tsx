808085ef5486faf1d9570436d1ff8803486be83e
Senthil.Gunasekaran <Senthil.Gunasekaran@KANINI-LTP-877>
2023-01-11 14:34:30 +0530

    DownLoadOptions

--- dttl.qr.presentation/src/App.tsx
+++ dttl.qr.presentation/src/App.tsx
@@ -3,19 +3,28 @@ import React from 'react';
 import './App.css';
 import TemplateComponent from './Components/TemplateComponent'
 import VCardComponent from './Components/VCardComponent';
+import { downloadQrCode } from './Utils/CanvasUtils';
 
 function App() {
     return (
         <><div className="App">
-
-            <TemplateComponent />
+            <svg id="testDownload" width="100" height="100">
+                <circle cx="50" cy="50" r="40" stroke="green" stroke-width="4" fill="yellow" />
+            </svg>
 
         </div>
             <div>
-
+                <TemplateComponent />
                 <VCardComponent />
 
-            </div></>
+            </div>
+            <button onClick={() => {
+                downloadQrCode("testDownload", "svg"); // png, jpeg and svg
+            }
+            }>Download</button>
+
+        </>
+
     );
 }
 
